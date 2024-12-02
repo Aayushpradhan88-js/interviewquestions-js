eafc326e6cb9583766fa036ac4984603615f2a79
aayush pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-12-02 14:09:53 +0000

    reduce

--- Javascript/Map,filter and reduce/reduce.js
+++ Javascript/Map,filter and reduce/reduce.js
@@ -1,7 +1,41 @@
 let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 let initialNumber = 0;
 
-let sumWithInitial = number.reduce((acc, currentVal) => acc + currentVal, initialNumber);
+let sumOfInitialNumber = number.reduce((accumalator, currentValue) => accumalator + currentValue, initialNumber)
 
-console.log(sumWithInitial);
+console.log(sumOfInitialNumber);
+
+//#Next method
+
+let digits = [4, 5, 6, 7, 8, 9];
+
+// let sum = digits.reduce((accumalator, currentValue) => {
+//     console.log(`acc: ${accumalator} and currVal: ${currentValue}`);
+//     return accumalator + currentValue;
+// },0)
+
+// let sum = digits.reduce((acc, curr) => acc + curr , 0);
+
+// let sum = digits.map((val) => {
+//     return val * 3;
+// })
+
+// let sum = digits
+//     .map((val) => val * 10)
+//     .filter((val) => val > 55)
+
+
+// console.log(sum);
+
+// Calculate the total price of products in a shopping cart, including quantity.
+const cart = [
+    { product: 'Shoes', price: 50, quantity: 2 },
+    { product: 'Shirt', price: 20, quantity: 3 }
+];
+
+let sum2 = cart.reduce((price, product)  => price + product.price * product.quantity , 0);
+
+console.log(`The sum of total price of the products is: ${sum2}`);
+
+let manipulatoin = digits.sort ();
+console.log(manipulatoin)
